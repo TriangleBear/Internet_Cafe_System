2f8fdae1f6ac414091d4d4445eb4b11072fe40eb
TriangleBear <kakanini31@gmail.com>
2022-09-19 20:58:39 +0800

    Create app.py
    app.py reveal

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, flash, request, session, redirect, url_for
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'internet_cafe_system'
+
+
+mysql = MySQL()
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'),code=302)
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    status=True
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        user_type='admin'
+        cur=mysql.connection.cursor()
+        cur.execute("select * from user where username=%s and password=%s and user_type=%s",(username,password,user_type))
+        data=cur.fetchone()
+        if data:
+            session['logged_in']=True
+            session['username']=data[1]
+            print('Login Successful!')
+            return redirect('home')
+        else:
+            print('username, password or user type invalid')
+    return render_template("login.html")
+
+@app.route('/home',methods=['POST','GET'])
+def home():
+    return render_template('home.html')
+
+
+
+@app.route('/home',methods=['POST','GET'])
+def report():
+    return render_template('home.html')
+
+
+@app.route('/logout',methods=['POST','GET'])
+def logout():
+    session.clear()
+    flash('You are now logged out','success')
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.secret_key = 'debugPhaseTotallyAccurateSecretKey69'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True)
+    
